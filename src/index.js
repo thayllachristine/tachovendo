6d06b5b6bfddb5afc9020930c5b169f67ac10738
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-02 21:56:04 -0200

    Alteração index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 
 import './index.scss'
 import Home from './pages/home'
+import SunnyDay from './pages/sunnyday';
+import RainyDay from './pages/rainyday';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
