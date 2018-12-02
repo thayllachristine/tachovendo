446799badea1824b5bd753dbe04897e63eff2aa5
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-02 00:35:07 -0200

    Adição de componente: Form

---
+++ src/components/form/form.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import Label from './label/label'
+import Input from './input/input'
+import Button from './button/button'
+
+
+const FormStyled = styled.form`
+    display: flex;
+    justify-content: center;
+`
+
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <FormStyled className='searchForm' onSubmit={this.onSubmit}>
+                {this.props.children}
+            </FormStyled>
+        )
+    }
+}
+
+
+Form.Label = Label
+Form.Input = Input
+Form.Button = Button
+
+
+export default Form
