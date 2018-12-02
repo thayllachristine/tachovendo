f93af85cd5faf9140619a069399b51e4b70eec3d
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-02 17:20:27 -0200

    Adição de componente: Input

---
+++ src/components/form/input/input.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+const InputStyled = styled.input`
+    width: 500px;
+    height: 50px;
+    border: 1px solid lightgray;
+    border-radius: 10px
+    
+    ::placeholder {
+        
+        color: gray;
+    }
+`
+
+
+export default class Input extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ``,
+        }
+    }
+
+
+    handleChange = (ev) => {
+        this.setState({ value: ev.target.value })
+        // const city = this.state.value
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <InputStyled
+                    type={this.props.type}
+                    id={this.props.id}
+                    className='searchInput'
+                    placeholder={this.props.placeholder}
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+            </React.Fragment>
+        )
+    }
+}
