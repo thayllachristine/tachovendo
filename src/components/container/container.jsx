06e8aa46277dce4ad27919d3e354e9fd73ad3ff4
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-02 00:24:42 -0200

    Adição de componente: Container

---
+++ src/components/container/container.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+const ContainerStyled = styled.div`
+    width: 900px;
+    height: 450px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+
+export default class Container extends Component {
+    render() {
+        return (
+            <ContainerStyled className='container'>
+                {this.props.children}
+            </ContainerStyled>
+        )
+    }
+}
