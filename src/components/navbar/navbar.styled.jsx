feb7e74da6482c65bd1b7f0f8d29a4d22cb8560a
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-03 21:13:14 -0200

    Adição de Styled Component: Navbar

---
+++ src/components/navbar/navbar.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const NavStyled = styled.nav`
+    // border-bottom: 1px solid lightgray;
+
+    background-color: transparent;
+    // width: 100%;
+    height: 50px;
+
+    padding: 20px;
+
+    
+`
+
+export default NavStyled
