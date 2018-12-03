baac45f244c0cc88c64f85a051f3cd948862109b
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-03 21:04:26 -0200

    Adição de Styled Component: Input

---
+++ src/components/form/input/input.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const InputSt = styled.input`
+    width: 350px;
+    height: 50px;
+    border: 1px solid #fff;
+    border-radius: 10px
+    
+    ::placeholder {
+        color: gray;
+    }
+`
