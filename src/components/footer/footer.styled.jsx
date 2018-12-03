3a73a2fd4da2ca2ab988bbff553e8f414b6d53bb
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-03 21:08:12 -0200

    Adição de Styled Component: Footer

---
+++ src/components/footer/footer.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const FooterSt = styled.footer`
+    height: 37px;
+    border-top: 1px solid black;
+
+    display: flex;
+    justify-content: center;
+    align-items:center;
+
+    background-color: #fff;
+`
+
+export const PhSt = styled.p`
+    font-size: 12px;
+`
