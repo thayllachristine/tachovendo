4e095a854b10fc5a55e54bf4447041ddc7bf2784
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-03 14:29:59 -0200

    Atualizção button.jsx

--- src/components/form/button/button.jsx
+++ src/components/form/button/button.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-
 import styled from 'styled-components'
 
 
 const ButtonStyle = styled.button`
     width: 50px;
     height: 50px;
-    border: .5px solid lightgray;
+    border: .5px solid #fff;
     border-radius: 10px;
     background-color: transparent;
 `
@@ -20,7 +19,7 @@ export default class Button extends Component {
     render() {
         return (
             <ButtonStyle>
-                <i class="material-icons">search</i>
+                {this.props.children}
             </ButtonStyle>
         )
     }
