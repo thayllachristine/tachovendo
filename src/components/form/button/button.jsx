f9f819f3fa1928794272e50961943d7eb5b204dc
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-03 21:01:19 -0200

    Alteração button.jsx

--- src/components/form/button/button.jsx
+++ src/components/form/button/button.jsx
@@ -1,26 +1,12 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-
-
-const ButtonStyle = styled.button`
-    width: 50px;
-    height: 50px;
-    border: .5px solid #fff;
-    border-radius: 10px;
-    background-color: transparent;
-`
-
+import ButtonSt from './button.styled'
 
 export default class Button extends Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return (
-            <ButtonStyle>
+            <ButtonSt>
                 {this.props.children}
-            </ButtonStyle>
+            </ButtonSt>
         )
     }
 }
