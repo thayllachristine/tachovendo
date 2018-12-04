9fcd1f57f1feac491b80567662210f36dd0080ff
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-04 17:01:02 -0200

    Atualização navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { removeLocalStorage } from '../../network/localStorage'
 import NavStyled from './navbar.styled'
-
 import Button from '../form/button/button'
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+
+    handleClick = (event) => {
+        event.preventDefault();
+        removeLocalStorage('city')
+        this.props.history.push('/home')
+    }
+
     render() {
         return (
             <NavStyled>
-                <Button>
-                    <i class="material-icons">keyboard_backspace</i>
-                </Button>
+                <a href='/home' onClick={this.handleClick}>
+                    <Button>
+                        <i className="material-icons">keyboard_backspace</i>
+                    </Button>
+                </a>
             </NavStyled>
         )
     }
 }
+
+export default withRouter(Navbar)
