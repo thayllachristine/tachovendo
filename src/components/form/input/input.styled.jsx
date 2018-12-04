613266898b7f591e79510a0ea750437caf8682ec
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-04 16:45:01 -0200

    Alteração input.styled.jsx

--- src/components/form/input/input.styled.jsx
+++ src/components/form/input/input.styled.jsx
@@ -1,12 +1,14 @@
 import styled from 'styled-components'
 
-export const InputSt = styled.input`
+const InputSt = styled.input`
     width: 350px;
     height: 50px;
-    border: 1px solid #fff;
+    border: 1px solid #ccc;
     border-radius: 10px
     
     ::placeholder {
         color: gray;
     }
 `
+
+export default InputSt
