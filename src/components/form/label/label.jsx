6874f88fa5f9c28244454fbe3451b8aebd1e7c0e
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-04 16:45:31 -0200

    Alteração label.jsx

--- src/components/form/label/label.jsx
+++ src/components/form/label/label.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import LabelSt from './label.styled'
 
 export default class Label extends Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return (
             <LabelSt htmlFor={this.props.htmlFor} >
