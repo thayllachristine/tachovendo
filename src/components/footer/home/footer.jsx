99bfbf2dcc2e331e7c156c0a6f3350288d3dbdc3
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-05 21:22:11 -0200

    Organização de componente Footer na página inicial

---
+++ src/components/footer/home/footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { FooterHome, FooterHomeP } from './footer.styled'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <FooterHome>
+                <FooterHomeP>
+                    Copyright 2018. Feito por Thaylla Christine
+                </FooterHomeP>
+            </FooterHome>
+        )
+    }
+}
---
+++ src/components/footer/home/footer.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const FooterHome = styled.footer`
+    padding: 13.5px;
+    display: flex;
+    justify-content: center;
+    
+    background-color: #cccccc78;
+`
+
+export const FooterHomeP = styled.p`
+    font-size: 10px;
+`
