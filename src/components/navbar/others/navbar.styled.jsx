afb8b9aff59b60843aa99e7e23a2f93002ba166f
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-05 21:10:59 -0200

    Organização de componente Navbar das outras páginas

---
+++ src/components/navbar/others/navbar.styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const NavStyled = styled.nav`
+    // border-bottom: 1px solid lightgray;
+
+    background-color: transparent;
+    // width: 100%;
+    height: 50px;
+
+    position: absolute;
+
+    padding: 20px;
+
+    
+`
+
+export default NavStyled
