7e4a313bb9d1a485e06bdeb3d730fe61b4fe1e02
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-05 21:12:20 -0200

    Organização de componente Navbar das outras páginas

---
+++ src/components/navbar/others/button/button.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ButtonSt = styled.button`
+    width: 50px;
+    height: 50px;
+    border: 1.5px solid;
+    border-radius: 10px;
+    background-color: transparent;
+`
+
+export default ButtonSt
