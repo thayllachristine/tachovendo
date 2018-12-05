62ac4adb0de037ca77f9fd09a95f3ff026b1adb8
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-05 17:22:37 -0200

    Adição de Styled Components: variáveis da página RainyDay

---
+++ src/pages/rainyday/rainyday.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const PSt = styled.p`
+    margin: 0 auto;
+    font-size: 25px;
+    color: #fff;
+`
+
+export const GridRow = styled.div`
+    height: 260px;
+    display: flex;
+`
+
+export const GridCol = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+`
