545a7cb3389a9eb6c4f4f47b49d19156fddd1961
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-05 21:10:19 -0200

    Organização de componente Navbar da página Home

---
+++ src/components/navbar/home/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { NavHome, PNav } from './navbar.styled'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavHome>
+                <PNav>tá chovendo?</PNav>
+            </NavHome>
+        )
+    }
+}
---
+++ src/components/navbar/home/navbar.styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const NavHome = styled.nav`
+    width: 100%;
+    height: 50px;
+    border-bottom: 1px solid #ccc;
+
+    display: flex;
+    align-items: center;
+`
+
+export const PNav = styled.p`
+    @import url('https://fonts.googleapis.com/css?family=Sacramento');
+    font-family: 'Sacramento', cursive;
+    font-size: 30px;
+    text-shadow: 2.5px 2.5px 1px #CCC;
+`
