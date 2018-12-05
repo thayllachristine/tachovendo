c9022d0e95bd87484e463b6d74fde8f0b9ce0172
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-05 21:15:08 -0200

    Organização de componente Footer das outras páginas

---
+++ src/components/footer/others/footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { FooterSt, PhSt } from './footer.styled'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <FooterSt>
+                <PhSt>
+                    Copyright 2018. Feito por Thaylla Christine.
+                </PhSt>
+            </FooterSt>
+        )
+    }
+}
