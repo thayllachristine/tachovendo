c67c6d5ffbb384bb877636305dd10959faf75c6a
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-06 16:32:03 -0200

    Alteração hgbrasil.jsx

--- src/resource/hgbrasil.jsx
+++ src/resource/hgbrasil.jsx
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
-import key from './CREDENTIAL_KEY'
-
 export default function weather(city) {
     return new Promise((resolve, reject) => {
-        axios.get(`https://api.hgbrasil.com/weather/?format=json-cors&city_name=${city}&key=${key.hgbrasil}`)
+        axios.get(`https://api.hgbrasil.com/weather/?format=json-cors&city_name=${city}&key=${process.env.REACT_APP_HGBRASIL_KEY}`)
             .then((resp) => {
                 resolve(resp)
             })
