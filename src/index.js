f17459056be1ae04f015e94075a0b956e58f42cf
Thaylla Christine <thayllachristineo@gmail.com>
2018-12-06 18:28:54 -0200

    Alteração index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import * as serviceWorker from './serviceWorker'
 import './index.scss'
 
 ReactDOM.render(
