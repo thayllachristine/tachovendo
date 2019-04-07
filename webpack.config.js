ecd9903b286d8eff63c32eae984d5e29a1776bb6
Thaylla Christine <thayllachristineo@gmail.com>
2019-04-07 19:12:31 -0300

    Altera configuração "spaces" para 2 no VSCode

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,19 @@ module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   output: {
-        path: path.resolve(__dirname, 'dist'), // ./dist
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader"
-            }]
-        }]
-    }
+    path: path.resolve(__dirname, 'dist'), 
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader"
+      }]
+    }]
+  }
 };
