79e008f817bd60366c06dbc5515b68e49eab693b
Thaylla Christine <thayllachristineo@gmail.com>
2019-04-07 19:10:03 -0300

    Altera configuração "spaces" para 2 no VSCode

--- src/resource/hgbrasil.jsx
+++ src/resource/hgbrasil.jsx
@@ -2,15 +2,14 @@ import axios from 'axios'
 import key from './CREDENTIAL_KEY';
 
 export default function weather(city) {
-    return new Promise((resolve, reject) => {
-        axios.get(`https://api.hgbrasil.com/weather/?format=json-cors&city_name=${city}&key=${process.env.REACT_APP_HGBRASIL_KEY}`)
-            .then((resp) => {
-                resolve(resp)
-            })
+  return new Promise((resolve, reject) => {
+    axios.get(`https://api.hgbrasil.com/weather/?format=json-cors&city_name=${city}&key=${process.env.REACT_APP_HGBRASIL_KEY}`)
+      .then((resp) => {
+        resolve(resp)
+      })
 
-            .catch((error) => {
-                reject(error)
-            })
-
-    })
+      .catch((error) => {
+        reject(error)
+      })
+  })
 }
