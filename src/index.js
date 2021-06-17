8322e5f85064c11f0da5153407ed938654d23e56
Thaylla Christine <thayllachristineo@gmail.com>
2021-06-17 14:47:16 -0300

    chore: remove serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 import './index.scss'
 
 ReactDOM.render(
@@ -12,5 +11,3 @@ ReactDOM.render(
 
     , document.getElementById('root'))
 
-
-serviceWorker.unregister()
