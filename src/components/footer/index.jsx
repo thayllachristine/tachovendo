8ebfd97949e0314a538b2b3048290680153d8783
Thaylla Christine <thayllachristineo@gmail.com>
2021-06-22 21:25:05 -0300

    refactor: remove internal prop

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import * as S from './Footer.styled';
 
-const Footer = ({ internal }) =>
-  internal ? (
-    <S.InternalFooter>
-      <p>Copyright 2018. Feito por Thaylla Christine.</p>
-    </S.InternalFooter>
-  ) : (
-    ''
-  );
+const Footer = () => (
+  <S.InternalFooter>
+    <p>Copyright 2018. Feito por Thaylla Christine.</p>
+  </S.InternalFooter>
+);
 
 export default Footer;
